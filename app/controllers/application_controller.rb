4e26ee8906d51d7f6944969fa2a0d1880443fd7b
Eric Storey <estorey@gmail.com>
2019-05-06 05:34:55 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,4 +24,10 @@ class ApplicationController < Sinatra::Base
     erb :edit
   end
   
+  post 'recipes' do 
+    @recipe=Recipe.new(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
+    @recipe.save
+    redirect ''
+  end
+  
 end
